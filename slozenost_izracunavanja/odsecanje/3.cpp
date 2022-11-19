b7d394a6ea61dd59b8e03e6a155ba024736cfed4
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-19 14:17:36 +0100

    serija najduzih pobeda

--- slozenost_izracunavanja/odsecanje/3.cpp
+++ slozenost_izracunavanja/odsecanje/3.cpp
@@ -1,10 +1,38 @@
 #include<iostream>
+#include<vector>
+#include<climits>
 
 using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
 
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int max = INT_MIN;
+    int duzina = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 1) {
+            duzina++;
+        } else {
+
+            if (duzina > max) {
+                max = duzina;
+            }
+
+            duzina = 0;
+        }
+    }
+
+    if (duzina > max)
+        max = duzina;
+
+    cout << max << endl;
 
 
     return 0;
